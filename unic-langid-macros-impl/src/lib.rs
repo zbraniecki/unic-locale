081c694e254a4516cb8beef6805e412560bd6ce2
Zibi Braniecki <zibi@braniecki.net>
2019-07-26 11:11:51 -0700

    Fix macro import error

--- unic-langid-macros-impl/src/lib.rs
+++ unic-langid-macros-impl/src/lib.rs
@@ -39,6 +39,6 @@ pub fn langid(input: TokenStream) -> TokenStream {
     };
 
     TokenStream::from(quote! {
-        ::unic_langid::LanguageIdentifier::from_parts(#lang, #script, #region, #variants).expect("must parse")
+        ::unic_langid_impl::LanguageIdentifier::from_parts(#lang, #script, #region, #variants).expect("must parse")
     })
 }
