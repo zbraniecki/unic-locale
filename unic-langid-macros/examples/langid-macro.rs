06b6eadefaa6d937a236b1bb85359b274504272e
Zibi Braniecki <zibi@braniecki.net>
2019-07-29 13:02:57 -0700

    Remove examples that no longer work because of the hackery

--- unic-langid-macros/examples/langid-macro.rs
+++
@@ -1,5 +0,0 @@
-use unic_langid_macros::langid;
-fn main() {
-    let id = langid!("de-Latn-DE");
-    println!("{:?}", id);
-}
--- unic-locale-macros/examples/locale-macro.rs
+++
@@ -1,7 +0,0 @@
-use unic_locale_impl::Locale;
-use unic_locale_macros::locale;
-
-fn main() {
-    let loc: Locale = locale!("de-Latn-DE");
-    println!("{:?}", loc);
-}
